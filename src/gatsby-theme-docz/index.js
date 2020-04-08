8f61c693acf1a66dce03feb8bc4c7bf4567a719d
Luo XiFeng <942394682@qq.com>
2020-04-09 02:44:41 +0800

    add demo

--- src/gatsby-theme-docz/index.js
+++ src/gatsby-theme-docz/index.js
@@ -1,6 +1,81 @@
 import React from 'react';
-import { theme } from 'docz';
+import { Styled, ThemeProvider } from 'theme-ui';
+import defaultTheme from 'gatsby-theme-docz/src/theme';
+import components from 'gatsby-theme-docz/src/components';
+import {
+  useCurrentDoc,
+  doczState,
+  useConfig,
+  ComponentsProvider
+} from 'docz';
+// import DefaultTheme from 'gatsby-theme-docz/src';
+// import * as R from 'ramda';
 
-const Theme = ({ children }) => <div>1111{children}</div>;
+/* eslint-disable */
 
-export default theme()(Theme);
+const DoczDefaultTheme = ({ children }) => {
+  const config = useConfig()
+  return (
+    <ThemeProvider theme={config.themeConfig}>
+      <ComponentsProvider components={components}>
+        <Styled.root>{children}</Styled.root>
+      </ComponentsProvider>
+    </ThemeProvider>
+  )
+};
+
+const MyTheme = ({ children }) => {
+
+  return (
+    <div>
+      {children}
+    </div>
+  );
+};
+
+
+function theme(themeConfig, transform = c => c) {
+  return WrappedComponent => {
+    const Theme = React.memo(props => {
+      const {
+        db,
+        currentEntry,
+        children
+      } = props;
+      const initial = Object.assign({}, db, {
+        currentEntry,
+        themeConfig,
+        transform
+      });
+
+      return (
+        <doczState.Provider initial={initial}>
+          <WrappedComponent>
+            {children}
+          </WrappedComponent>
+        </doczState.Provider>
+      )
+
+      // return React.createElement(doczState.Provider, {
+      //   initial: initial
+      // }, React.createElement(WrappedComponent, null, children));
+    });
+    Theme.displayName = WrappedComponent.displayName || 'DoczTheme';
+    return Theme;
+  };
+}
+
+const SwitchTheme = ({ children }) => {
+  const currentDoc = useCurrentDoc() || {};
+  const { route = '' } = currentDoc;
+
+  console.log(currentDoc);
+
+  if (route.startsWith('/demo')) {
+    return <MyTheme children={children} />;
+  }
+
+  return <DoczDefaultTheme children={children} />;
+}
+
+export default theme(defaultTheme)(SwitchTheme);
