ab5683867ad632950a04d01ed9f5a98587259e8c
luoxifeng <942394682@qq.com>
2020-04-13 21:46:50 +0800

    transform.js

--- transform.js
+++ transform.js
@@ -1,23 +1,20 @@
 const globby = require('globby');
 const { resolve } = require('path');
 const R = require('ramda');
-const { from } = require('rxjs');
+const { from, of } = require('rxjs');
 const { map, mergeMap } = require('rxjs/operators');
 const fs = require('fs-extra');
 
+// let index = 0;
 from(globby('./**/*.mdx'))
   .pipe(
     map(R.map(path => resolve(__dirname, path))),
-    mergeMap((paths) => {
-      return paths.map(path => {
-        return from(fs.readFile(path)).pipe(
-          mergeMap(content => {
-            path.replace(/demo/, '.preview');
-          })
-        );
-      });
+    mergeMap(paths => {
+      return from(paths).pipe(
+        mergeMap(path => fs.readFile(path))
+      );
     })
   )
-  .subscribe((dd) => {
-    console.log(dd);
+  .subscribe(dd => {
+    console.log(dd.toString());
   });
