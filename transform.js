711a4fddce5dcb2bd065966004dda099424af5b9
Luo XiFeng <942394682@qq.com>
2020-04-14 00:41:25 +0800

    add transform

--- transform.js
+++ transform.js
@@ -1,20 +1,31 @@
 const globby = require('globby');
 const { resolve } = require('path');
 const R = require('ramda');
-const { from, of } = require('rxjs');
+const { from } = require('rxjs');
 const { map, mergeMap } = require('rxjs/operators');
 const fs = require('fs-extra');
 
-// let index = 0;
 from(globby('./**/*.mdx'))
   .pipe(
+    /**
+     * 1.获取真实路径
+     */
     map(R.map(path => resolve(__dirname, path))),
+
+    /**
+     * 2.进行文件内容转换并写入新文件
+     */
     mergeMap(paths => {
       return from(paths).pipe(
-        mergeMap(path => fs.readFile(path))
+        mergeMap(async path => {
+          const content = await fs.readFile(path);
+          const targetPath = path.replace(/components/, '.preview');
+          await fs.ensureFile(targetPath);
+          return fs.writeFile(targetPath, content);
+        })
       );
     })
   )
   .subscribe(dd => {
-    console.log(dd.toString());
+    // console.log(dd.toString());
   });
